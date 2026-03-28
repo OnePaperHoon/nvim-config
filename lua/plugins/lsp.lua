ecc46c10e3e9f1961ab27d85d7ae6e3db13fd7d1
OnePaperHoon <wkdgns720@naver.com>
2026-03-28 23:34:09 +0900

    fix version

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,14 +5,32 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- cmp-nvim-lsp capabilities
+      local ok_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local capabilities = ok_cmp and cmp_nvim_lsp.default_capabilities() or {}
 
-      vim.lsp.config("clangd", {
-        capabilities = capabilities,
-      })
+      -- Neovim 버전 체크
+      local nvim_version = vim.version()
+      local is_011_or_later = (nvim_version.major > 0) or 
+                              (nvim_version.major == 0 and nvim_version.minor >= 11)
 
-      vim.lsp.enable("clangd")
+      if is_011_or_later then
+        -- Neovim 0.11+ 방식 (맥)
+        vim.lsp.config("clangd", {
+          capabilities = capabilities,
+        })
+        vim.lsp.enable("clangd")
+      else
+        -- Neovim 0.10 방식 (라즈베리파이)
+        local ok_lsp, lspconfig = pcall(require, "lspconfig")
+        if ok_lsp and vim.fn.executable("clangd") == 1 then
+          lspconfig.clangd.setup({
+            capabilities = capabilities,
+          })
+        end
+      end
 
+      -- 키맵 설정 (공통)
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
       vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
@@ -23,3 +41,4 @@ return {
     end,
   },
 }
+
